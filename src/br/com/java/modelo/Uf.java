41e221bbfb9e3d5ac7e774cdfaafbedae0dbb422
paulo.gomes <paulo.gomes@arce.ce.gov.br>
2012-11-22 16:20:58 -0300

    Merge ajuste de conflitos

---
+++ src/br/com/java/modelo/Uf.java
@@ -0,0 +1,90 @@
+package br.com.java.modelo;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_uf")
+@NamedQueries({
+	@NamedQuery(name = "ufListarTodos", query = "SELECT u FROM Uf u"),
+	@NamedQuery(name = "ufPesquisarPorId", query = "SELECT u FROM Uf u WHERE u.id = :id"),
+})
+public class Uf implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String sigla;
+	private String nome;
+
+	@Id
+	@SequenceGenerator(name = "seq_id_uf", sequenceName = "seq_id_uf", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_id_uf")
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla.toUpperCase();
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sigla == null) ? 0 : sigla.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Uf other = (Uf) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sigla == null) {
+			if (other.sigla != null)
+				return false;
+		} else if (!sigla.equals(other.sigla))
+			return false;
+		return true;
+	}
+	
+}
