c0d91ee6de7740b5fee06bc7369e8b5c290acace
felipe <felipe@localhost>
2013-04-10 17:07:35 -0300

    Add cadastro de produto;

--- src/br/com/jave/managedbean/ProdutoMB.java
+++ src/br/com/jave/managedbean/ProdutoMB.java
@@ -1,9 +1,12 @@
 package br.com.jave.managedbean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.NoResultException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,6 +26,8 @@ public class ProdutoMB implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private ProdutoDao produtoDao;
 	private Produto produto = new Produto();
+	private Produto produtoSelection = new Produto();
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public ProdutoMB() {}
 	
@@ -30,18 +35,51 @@ public class ProdutoMB implements Serializable{
 	@Autowired
 	public ProdutoMB(ProdutoDao produtoDao) {
 		this.produtoDao = produtoDao;	
-	
 	}
 
-	public void gravar() throws Exception {
-		produtoDao.gravar(produto);
+	public void gravar() {
+		try {
+			produtoDao.gravar(produto);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		FacesMessageUtil.mensagem("Produto gravado com sucesso!");
 	}
+	
+	public void prepararEdicao() {
+		try {
+			
+			produto = produtoDao.pesquisarPorId(produtoSelection.getId());
+		} catch (NoResultException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public List<Produto> listarProdutos(){
+		
+		try {
+			produtos = produtoDao.listarTodos();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return produtos;
+	}
+	
 	
 	public String criarNovo(){
 		produto = new Produto();
 		return "novo";
 	}
+	
+	
+	
 	
 	public ProdutoDao getProdutoDao() {
 		return produtoDao;
@@ -61,9 +99,26 @@ public class ProdutoMB implements Serializable{
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
-	
-	
-	
+
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+
+	public Produto getProdutoSelection() {
+		return produtoSelection;
+	}
+
+
+	public void setProdutoSelection(Produto produtoSelection) {
+		this.produtoSelection = produtoSelection;
+	}
+		
 	
 }
